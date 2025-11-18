27c7fec2164cc1a63c2cf0da26aa75397bdd0190
kadirkhan02 <kadirkhanamt@gmail.com>
2025-11-18 23:57:55 +0530

    First Commit

--- src/main/java/com/dep/depApp/Service/DepartmentService.java
+++ src/main/java/com/dep/depApp/Service/DepartmentService.java
@@ -6,13 +6,16 @@ import com.dep.depApp.entity.Department;
 import com.dep.depApp.repository.DepartmentRepository;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.jspecify.annotations.Nullable;
+import org.springframework.util.ReflectionUtils;
 import org.modelmapper.ModelMapper;
+//import org.springframework.data.util.ReflectionUtils;
 import org.springframework.stereotype.Service;
-
+import java.lang.reflect.Field;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
-
+import org.springframework.util.ReflectionUtils;
 @Service
 @RequiredArgsConstructor
 public class DepartmentService {
@@ -32,5 +35,70 @@ public class DepartmentService {
         Department d=mp.map(requestDepartmentDto,Department.class);
         Department saved=departmentRepository.save(d);
         return mp.map(saved,ResponseDepartmentDto.class);
+    }
+
+    public ResponseDepartmentDto updateDepartment(RequestDepartmentDto requestDepartmentDto, long id) {
+
+        checkdepartmentById(id);
+        Department d=departmentRepository.findById(id).get();
+        Department department=mp.map(requestDepartmentDto,Department.class);
+        department.setId(id);
+        Department saved=departmentRepository.save(department);
+        return mp.map(saved,ResponseDepartmentDto.class);
+    }
+
+    public boolean checkdepartmentById(Long id)
+    {
+        boolean exists=departmentRepository.existsById(id);
+        if (!exists) {
+            throw new IllegalArgumentException("Department with id " + id + " does not exist");
+        }
+        return true;
+    }
+
+    public boolean deleteDepartment(long id) {
+        checkdepartmentById(id);
+        departmentRepository.deleteById(id) ;
+        return true;
+    }
+    private Object convertValue(Class<?> targetType, Object value) {
+
+        if (targetType == LocalDateTime.class) {
+            return LocalDateTime.parse(value.toString());
+        }
+        if (targetType == Boolean.class || targetType == boolean.class) {
+            return Boolean.parseBoolean(value.toString());
+        }
+        if (targetType == Long.class || targetType == long.class) {
+            return Long.parseLong(value.toString());
+        }
+
+        return value;
+    }
+
+    public ResponseDepartmentDto updatePartially(Map<String, Object> mpp, long id) {
+        checkdepartmentById(id);
+
+        Department department = departmentRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Department not found"));
+
+        mpp.forEach((field, value) -> {
+
+            Field fieldToBeUpdated = ReflectionUtils.findField(Department.class, field);
+
+            if (fieldToBeUpdated == null) {
+                throw new IllegalArgumentException("Invalid field: " + field);
+            }
+
+            fieldToBeUpdated.setAccessible(true);
+
+            Object convertedValue = convertValue(fieldToBeUpdated.getType(), value);
+
+            ReflectionUtils.setField(fieldToBeUpdated, department, convertedValue);
+        });
+
+        Department saved = departmentRepository.save(department);
+
+        return mp.map(saved, ResponseDepartmentDto.class);
     }
 }
--- src/main/java/com/dep/depApp/controller/DepartmentController.java
+++ src/main/java/com/dep/depApp/controller/DepartmentController.java
@@ -3,11 +3,13 @@ package com.dep.depApp.controller;
 import com.dep.depApp.DTO.RequestDepartmentDto;
 import com.dep.depApp.DTO.ResponseDepartmentDto;
 import com.dep.depApp.Service.DepartmentService;
+import com.dep.depApp.entity.Department;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 import static org.springframework.web.servlet.function.ServerResponse.ok;
 
@@ -24,7 +26,7 @@ public class DepartmentController {
         this.departmentService=departmentService;
     }
 
-    @GetMapping("/getall")
+    @GetMapping()
     public ResponseEntity<List<ResponseDepartmentDto>> getAlldepartment()
     {
         return ResponseEntity.ok(departmentService.getAlldepartment());
@@ -34,5 +36,27 @@ public class DepartmentController {
     public ResponseEntity<ResponseDepartmentDto>add(@RequestBody @Valid RequestDepartmentDto requestDepartmentDto)
     {
         return ResponseEntity.ok(departmentService.add(requestDepartmentDto));
+    }
+    @PutMapping(path="/{id}")
+    public ResponseEntity<ResponseDepartmentDto>updatedepartment(@RequestBody RequestDepartmentDto requestDepartmentDto,@PathVariable long id)
+    {
+        return ResponseEntity.ok(departmentService.updateDepartment(requestDepartmentDto,id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Boolean> deleteDepartment(@PathVariable long id)
+    {
+      boolean deleted=departmentService.deleteDepartment(id);
+      if(!deleted)
+      {
+          return ResponseEntity.notFound().build();
+      }
+      return ResponseEntity.ok(true);
+    }
+
+    public ResponseEntity<ResponseDepartmentDto>updatePartially(@RequestBody Map<String,Object>mp ,@PathVariable long id)
+    {
+        ResponseDepartmentDto responseDepartmentDto=departmentService.updatePartially(mp,id);
+        return null;
     }
 }
