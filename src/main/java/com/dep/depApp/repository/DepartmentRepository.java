b4eb9d6a48ef451cb8f46f962b8f0f8edf63a4d4
kadirkhan02 <kadirkhanamt@gmail.com>
2025-11-27 00:12:45 +0530

    First Commit

--- src/main/java/com/dep/depApp/repository/DepartmentRepository.java
+++ src/main/java/com/dep/depApp/repository/DepartmentRepository.java
@@ -2,6 +2,11 @@ package com.dep.depApp.repository;
 
 import com.dep.depApp.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface DepartmentRepository extends JpaRepository<Department,Long> {
+    Optional<Department> findByRoles(String hrAdmin);
 }
--- src/test/java/com/dep/depApp/DepAppApplicationTests.java
+++ src/test/java/com/dep/depApp/DepAppApplicationTests.java
@@ -1,13 +1,26 @@
 package com.dep.depApp;
 
+import com.dep.depApp.entity.Department;
+import com.dep.depApp.repository.DepartmentRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
 
 @SpringBootTest
 class DepAppApplicationTests {
 
-	@Test
+	@Autowired
+    DepartmentRepository departmentRepository;
+    @Test
 	void contextLoads() {
 	}
 
+    @Test
+    void testapp()
+    {
+        Optional<Department> department=departmentRepository.findByRoles("HR_ADMIN");
+        department.ifPresent(System.out::println);
+    }
 }
